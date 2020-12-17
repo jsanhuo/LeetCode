4f69e7bf88a60b2b93b3e6bd6222adbf1854796b
jiaoyan <1483104508@qq.com>
2020-12-17 22:12:52 +0800

    ac714

---
+++ python/714.py
@@ -0,0 +1,8 @@
+#time:884ms;memory:21.4MB
+class Solution:
+    def maxProfit(self, prices: List[int], fee: int) -> int:
+        n = len(prices)
+        sell, buy = 0, -prices[0]
+        for i in range(1, n):
+            sell, buy = max(sell, buy + prices[i] - fee), max(buy, sell - prices[i])
+        return sell
