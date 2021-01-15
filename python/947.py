07112e7181a597c690505b18eb637eb00c979f19
jiaoyan <1483104508@qq.com>
2021-01-15 10:14:51 +0800

    ac947

--- python/947.py
+++ python/947.py
@@ -29,3 +29,31 @@ class Solution:
                 dfs(i)
         
         return len(stones) - ans
+
+#time:68ms;memory:15.2MB
+class Solution:
+    def removeStones(self, stones: List[List[int]]) -> int:
+
+        self.count = 0
+        # 定义并查集
+        uf = defaultdict(int)
+        # 压缩路径
+        def find(x):
+            if(uf[x]==0):
+                uf[x] = x
+                self.count+=1
+            if(x!=uf[x]):
+                uf[x] = find(uf[x])
+            # 返回x的根节点
+            return uf[x]
+        def union(x,y):
+            xroot = find(x)
+            yroot = find(y)
+            if(xroot == yroot):
+                return
+            # 合并
+            uf[xroot] = yroot
+            self.count-=1
+        for x,y in stones:
+            union(x+20000,y+10000)
+        return len(stones) - self.count
