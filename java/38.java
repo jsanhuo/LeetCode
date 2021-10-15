56b7f865755355b263c6e676f7494636ac2ae026
SuShe\JiaoYan <1483104508@qq.com>
2021-10-15 23:41:55 +0800

    每日一题再做一遍！

--- java/38.java
+++ java/38.java
@@ -21,3 +21,33 @@ class Solution {
     	return sb.toString();
     }
 }
+//time:8ms;memory:38.3MB
+class Solution {
+    public String countAndSay(int n) {
+        if(n==1){
+            return "1";
+        }
+        String a = "1";
+        for(int i=2;i<=n;i++){
+            a = desc(a);
+        }
+        return a;
+    }
+    public String desc(String a){
+        char[] aa = a.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        int num = 1;
+        char x = aa[0];
+        for(int i=1;i<aa.length;i++){
+            if(x!=aa[i]){
+                sb.append(num+""+x);
+                num = 1;
+                x = aa[i];
+            }else{
+                num++;
+            }
+        }
+        sb.append(num+""+x);
+        return sb.toString();
+    }
+}
