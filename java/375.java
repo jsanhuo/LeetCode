b5a42309e090e24ecb31ad1b54d57233f8a9e8d6
jiaoyan <1483104508@qq.com>
2021-11-13 00:00:48 +0800

    优化

--- java/375.java
+++ java/375.java
@@ -3,12 +3,16 @@ class Solution {
         int[][] dp = new int[n + 1][n + 1];
         for (int i = n - 1; i >= 1; i--) {
             for (int j = i + 1; j <= n; j++) {
-                int minCost = Integer.MAX_VALUE;
-                for (int k = i; k < j; k++) {
-                    int cost = k + Math.max(dp[i][k - 1], dp[k + 1][j]);
-                    minCost = Math.min(minCost, cost);
-                }
-                dp[i][j] = minCost;
+                if(i == j+1){
+                    dp[i][j] = i;
+                }else{
+                    int minCost = Integer.MAX_VALUE;
+                    for (int k = i; k < j; k++) {
+                        int cost = k + Math.max(dp[i][k - 1], dp[k + 1][j]);
+                        minCost = Math.min(minCost, cost);
+                    }
+                    dp[i][j] = minCost;
+                } 
             }
         }
         return dp[1][n];
