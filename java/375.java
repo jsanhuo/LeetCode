d9e4b90eb4bda116f764291b6fc3e6bf96389061
jiaoyan <1483104508@qq.com>
2021-11-12 23:51:20 +0800

    每日一题

---
+++ java/375.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int getMoneyAmount(int n) {
+        int[][] dp = new int[n + 1][n + 1];
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = i + 1; j <= n; j++) {
+                int minCost = Integer.MAX_VALUE;
+                for (int k = i; k < j; k++) {
+                    int cost = k + Math.max(dp[i][k - 1], dp[k + 1][j]);
+                    minCost = Math.min(minCost, cost);
+                }
+                dp[i][j] = minCost;
+            }
+        }
+        return dp[1][n];
+    }
+}
