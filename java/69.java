2d127ee4c792373ed93cdb0e16d6ab894930daf8
jiaoyan <1483104508@qq.com>
2021-12-23 23:47:09 +0800

    每日一题

---
+++ java/69.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int mySqrt(int x) {
+        if (x == 0) {
+            return 0;
+        }
+
+        double C = x, x0 = x;
+        while (true) {
+            double xi = x0 -(x0*x0 -C)/(2*x0);
+            if (Math.abs(x0 - xi) < 1e-7) {
+                break;
+            }
+            x0 = xi;
+        }
+        return (int) x0;
+    }
+}
